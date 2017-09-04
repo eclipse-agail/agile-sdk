b2e8bf0ed08e39c9ade645b8d40cf4a5b9227ad4
craig-mulligan <craig@resin.io>
2017-09-04 14:28:30 -0400

    Add tokenGet, TokenDelete
    Change-Type: patch

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -3,9 +3,7 @@ import agileSDK from '../src/';
 import _ from 'lodash';
 
 const HOST = 'http://localhost:8080/';
-const agile = agileSDK({
-  api: HOST
-});
+const agile = agileSDK(HOST);
 const base = 'api/device/';
 const deviceId = 'bleB0B448BE5084';
 const expect = m.chai.expect;
@@ -42,5 +40,26 @@ describe('protocolManager', function() {
       return expect(promise).to.eventually.be.an('Array');
     });
   });
+});
 
+describe('token utils', function () {
+  describe('.tokenSet', function () {
+    it('expect to return token', function() {
+      expect(agile.tokenSet('1234')).to.equal('1234');
+    });
+  });
+
+  describe('.tokenGet', function () {
+    it('expect to return token', function() {
+      agile.tokenSet('4567');
+      expect(agile.tokenGet()).to.equal('4567');
+    });
+  });
+
+  describe('.tokenDelete', function () {
+    it('expect to return token', function() {
+      agile.tokenDelete();
+      expect(agile.tokenGet()).to.equal(undefined);
+    });
+  });
 });
