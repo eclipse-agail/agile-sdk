5645d610723888f0378085f6bd94261cac1b0fa9
Csaba Kiraly <csaba.kiraly@gmail.com>
2018-02-15 15:19:21 +0100

    EPL 2.0 license
    Change-type: patch
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- examples/token_conf.js
+++ examples/token_conf.js
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (C) 2018 resin.io, and others
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 module.exports = {
   token: '6c9GnAEFHZqKNYQJyAxo1bK1Z6HQz8Ol1Sx1CiQmBBICzQiepjinycaVTENv4Zly',
   api: 'http://passaugw.local:8080',
