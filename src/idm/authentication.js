f4aa4e87a9c3d46aed2e01ba53ca7032b156e45d
dp <dp>
2018-09-11 17:09:39 +0200

    add user authentication with oauth2 call and its example
    Change-Type: minor

--- src/idm/authentication.js
+++ src/idm/authentication.js
@@ -44,6 +44,44 @@ const entity = (base, token) => {
         data: { grant_type: 'client_credentials' }
       });
     }
+  },{
+  /**
+  * @summary Authenticate a user for a particular client (application) relying on agile-security for user authentication.
+  * @name authenticateUser
+  * @public
+  * @function
+  * @memberof agile.idm.authentication
+  * @param {String} client - client name. This is the client name provided to the create Entity when you register an Oauth2 client in AGILE-IDM. For more info: https://github.com/Agile-IoT/agile-idm-oauth2-client-example
+  * @param {String} secret - client secret. This is the client name provided to the create Entity when you register an Oauth2 client in AGILE-IDM. For more info: https://github.com/Agile-IoT/agile-idm-oauth2-client-example
+  * @param {String} username - username for agile-local user
+  * @param {String} password - password for the agile-local user 
+  * @fulfil {Object} Authentication information including token_type and access_token
+  * @returns {Promise}
+  * @example
+  * agile.idm.authentication.authenticateClient('MyAgileClient2','WLnhhc3LnesbYj0GspNA13zgJEroN8V').then(function(result) {
+  *   console.log(credentials.access_token);
+  *   console.log(credentials.token_type);
+  * });
+  **/
+  authenticateUser: (client, secret, username, password) => {
+    let url = `${base}/oauth2/token`;
+    return axios.request({
+      method: 'POST',
+      url: url,
+      auth: {
+        username: client,
+        password: secret
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        grant_type: 'client_credentials',
+        username: username,
+        password : password
+      }
+    });
+  }
   });
 };
 
